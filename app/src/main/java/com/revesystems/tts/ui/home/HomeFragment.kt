f6d51087c2ea5c4045c2ec252212bbcc9f385e9c
imranrana <imranrana07@gmail.com>
2022-09-21 13:13:40 +0600

    pdf showing in the app successfully"

--- app/src/main/java/com/revesystems/tts/ui/home/HomeFragment.kt
+++ app/src/main/java/com/revesystems/tts/ui/home/HomeFragment.kt
@@ -1,24 +1,80 @@
 package com.revesystems.tts.ui.home
 
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.os.FileUtils
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.lifecycleScope
-import com.revesystems.tts.R
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import com.revesystems.tts.core.BaseFragment
-import com.revesystems.tts.data.source.local.datastore.DataStore
-import com.revesystems.tts.data.source.local.datastore.DataStore.getStringValF
 import com.revesystems.tts.databinding.FragmentHomeBinding
-import com.revesystems.tts.utils.toast
-import kotlinx.coroutines.launch
+import com.revesystems.tts.utils.GONE
+import com.revesystems.tts.utils.VISIBLE
+import java.io.File
 
 class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>() {
+
+    // Initialize result launcher
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        Log.v("ADSADA","")
+        if (it.resultCode == RESULT_OK && it != null) {
+            controlVisibility(true)
+            binding.pdfViewer.fromUri(it.data?.data!!).load()
+        }else{
+            controlVisibility(false)
+        }
+    }
+
     override fun setLayout(inflater: LayoutInflater, container: ViewGroup?): FragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
     override fun setViewModel(): Class<HomeViewModel>  = HomeViewModel::class.java
 
     override fun init(savedInstanceState: Bundle?) {
+        binding.etText.addTextChangedListener(tvWatcher)
+        clickEvents()
+    }
 
+    private fun clickEvents(){
+        binding.btnSelectPdf.setOnClickListener { selectPdf() }
+    }
+
+    private val tvWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun afterTextChanged(p0: Editable?) {
+            binding.tvLetterCount.text = "${p0.toString().length}/2500"
+        }
+    }
+
+    private fun selectPdf(){
+        val pdfIntent = Intent(Intent.ACTION_GET_CONTENT)
+        pdfIntent.type = "application/pdf"
+        pdfIntent.addCategory(Intent.CATEGORY_OPENABLE)
+        resultLauncher.launch(pdfIntent)
+    }
+
+    private fun controlVisibility(shouldVisible:Boolean){
+        if (shouldVisible) {
+            binding.pdfViewer.visibility = VISIBLE
+            binding.tlText.visibility = GONE
+            binding.tvLetterCount.visibility = GONE
+        }else{
+            binding.pdfViewer.visibility =GONE
+            binding.tlText.visibility = VISIBLE
+            binding.tvLetterCount.visibility = VISIBLE
+        }
     }
 }
